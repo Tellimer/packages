42e12d9782daa4463f0ac575450f1bbbf8fd63e3
Tim Feid <tim@timfeid.com>
2022-01-19 11:36:55 -0500

    fix: more changes

--- mail-app/src/emails/welcome.mailable.tsx
+++ mail-app/src/emails/welcome.mailable.tsx
@@ -12,40 +12,220 @@ export class WelcomeMailable extends Email {
   email() {
     return (
       <>
-        <EmailSection center padBottom={16} className="text-4xl">
-          Welcome to Scriber, :name!
+<EmailSection padBottom={24}>
+          <span className="post-title">This is the post title, should look good on multiple lines</span>
         </EmailSection>
-        <EmailSection center pad={16}>
-          <p>
-            You've just created your publication on Scriber, and joined a growing community of content creators.
-          </p>
-          <p>
-            We're thrilled to have you - now it's time to start writing.
-          </p>
+        <EmailSection padBottom={24}>
+          <EmailTable>
+            <EmailRow>
+              <EmailCol width={56}>
+                <EmailImage
+                  className="creator-image"
+                  src="https://cdn.scriber.tellimer.io/files/avatar-example.png"
+                  alt="Tom Cook"
+                  width={40}
+                  height={40}
+                ></EmailImage>
+              </EmailCol>
+              <EmailCol>
+                <EmailTable>
+                  <EmailRow>
+                    <EmailCol
+                      className="creator-name"
+                      style={{ lineHeight: '20px', color: '#111827' }}
+                    >
+                      Tom Cook
+                    </EmailCol>
+                  </EmailRow>
+                  <EmailRow>
+                    <EmailCol style={{ lineHeight: '20px', color: '#374151' }}>
+                      A day - 2 Min read
+                    </EmailCol>
+                  </EmailRow>
+                </EmailTable>
+              </EmailCol>
+            </EmailRow>
+          </EmailTable>
         </EmailSection>
-        <EmailSection center pad={16}>
-          <EmailDesktopOnly>
-            <EmailAutosizeButton align="center" title="Write your first post" background="#4F46E5" href="#" />
-          </EmailDesktopOnly>
-          <EmailMobileOnly>
-            <EmailAutosizeButton align="center" fontSize={20} title="Write your first post" background="#4F46E5" href="#" />
-          </EmailMobileOnly>
-        </EmailSection>
-        <EmailSection pad={16}>
         <div
-                dangerouslySetInnerHTML={{__html: `<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos officia cupiditate blanditiis neque consequatur repudiandae </p><blockquote><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos officia cupiditate bland</p></blockquote><p>perferendis pariatur nihil! Eveniet quas nulla quam laboriosam veritatis unde consectetur illo autem repudiandae dolorum.</p><ul><li><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos officia cupiditate bland</p></li><li><p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi eos quas rem ipsam vitae quia pariatur, hic neque</p></li><li><p>Eaque officia sit repellat delectus amet, numquam alias mollitia est iusto. Nisi?</p></li></ul><h2>This is a heading</h2><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos officia cupiditate blanditiis neque <a target="_blank" rel="noopener noreferrer nofollow" href="https://localhost.com/api/graphql">here is a link</a> consequatur repudiandae perferendis pariatur nihil! Eveniet quas nulla quam laboriosam veritatis unde consectetur illo autem repudiandae dolorum.</p><ol><li><p>ordered</p></li><li><p>list</p></li><li><p>here</p></li><li><p>another one</p></li></ol><h3>A subheading</h3><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos officia cupiditate blanditiis neque consequatur repudiandae perferendis pariatur nihil! Eveniet quas nulla quam laboriosam veritatis unde consectetur illo autem repudiandae dolorum.</p><figure><img src="https://d1e23yf52y86uu.cloudfront.net/files/hkyk82q2s-1r.jpg" alt="" title="This is a building somewhere in the states"><figcaption>This is a building somewhere in the states</figcaption></figure><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem pariatur, libero quia nisi architecto provident consequatur quod, quasi aliquid officia animi recusandae ducimus? Vitae, sequi voluptatem. Expedita cum excepturi aut?</p>`}}>
-              </div>
-        </EmailSection>
-        <EmailSection pad={16}>
-
+          className="post-content"
+          dangerouslySetInnerHTML={{
+            __html: `<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos officia cupiditate blanditiis neque consequatur repudiandae </p><blockquote><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos officia cupiditate bland</p></blockquote><p>perferendis pariatur nihil! Eveniet quas nulla quam laboriosam veritatis unde consectetur illo autem repudiandae dolorum.</p><ul><li><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos officia cupiditate bland</p></li><li><p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi eos quas rem ipsam vitae quia pariatur, hic neque</p></li><li><p>Eaque officia sit repellat delectus amet, numquam alias mollitia est iusto. Nisi?</p></li></ul><h2>This is a heading</h2><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos officia cupiditate blanditiis neque <a target="_blank" rel="noopener noreferrer nofollow" href="https://localhost.com/api/graphql">here is a link</a> consequatur repudiandae perferendis pariatur nihil! Eveniet quas nulla quam laboriosam veritatis unde consectetur illo autem repudiandae dolorum.</p><ol><li><p>ordered</p></li><li><p>list</p></li><li><p>here</p></li><li><p>another one</p></li></ol><h3>A subheading</h3><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos officia cupiditate blanditiis neque consequatur repudiandae perferendis pariatur nihil! Eveniet quas nulla quam laboriosam veritatis unde consectetur illo autem repudiandae dolorum.</p><figure><img width=520 style="width: 520px" src="https://d1e23yf52y86uu.cloudfront.net/files/hkyk82q2s-1r.jpg" alt="" title="This is a building somewhere in the states"><figcaption>This is a building somewhere in the states</figcaption></figure><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem pariatur, libero quia nisi architecto provident consequatur quod, quasi aliquid officia animi recusandae ducimus? Vitae, sequi voluptatem. Expedita cum excepturi aut?</p>`
+          }}
+        ></div>
+        <EmailSection padTop={24}>
           <EmailDesktopOnly>
-            <EmailAutosizeButton style='outline' align="center" fontSize={14} title="Write your first post" color="#111827" background="#D1D5DB" href="#" />
+            <EmailAutosizeButton
+              title="View more articles"
+              background={'#000000'}
+              href="#"
+            />
           </EmailDesktopOnly>
           <EmailMobileOnly>
-            <EmailAutosizeButton style='outline' align="center" fontSize={20} title="Write your first post" color="#111827" background="#D1D5DB" href="#" />
+            <EmailAutosizeButton
+              title="View more articles"
+              fontSize={22}
+              background={'#000000'}
+              href="#"
+            />
           </EmailMobileOnly>
         </EmailSection>
       </>
     )
+  }
+
+  mobileCss() {
+    return `
+
+    .email-social-image {
+      width: 32px !important;
+      height: 32px !important;
+    }
+      .creator-image {
+        width: 64px !important;
+        height: 64px !important;
+        margin-right: 16px !important;
+      }
+
+      .creator-name {
+        line-height: 34px !important;
+      }
+
+      h2 {
+        font-size: 28px !important;
+      }
+
+      h3 {
+        font-size: 24px !important;
+      }
+
+      .image-caption {
+        font-size: 20px !important;
+      }
+
+      .post-content > div,
+      .post-content > p,
+      .post-content > span,
+      .post-content li {
+        line-height: 32px !important;
+      }
+
+      .post-content > p,
+      .image-figure,
+      .email-quote {
+        margin-bottom: 32px !important;
+      }
+    `
+  }
+
+  css() {
+    return `
+      body,
+      p,
+      li span,
+      h2,
+      h3,
+      div {
+        color: #111827;
+      }
+
+      body {
+        background: #ffffff;
+      }
+
+      .text-4xl {
+        color: #111827;
+      }
+
+      .text-lg {
+        font-size: 20px;
+        line-height: 28px;
+      }
+
+      .article-content {
+        color: #6B7280;
+        line-height: 26px;
+      }
+      .article-content p,
+      .article-content li,
+      .article-content div {
+        line-height: 26px;
+      }
+
+      td p {
+        line-height: 24px;
+      }
+
+      .email-quote {
+        border-color: #0D9488;
+      }
+
+      a {
+        color: #066B64;
+        text-decoration: none;
+      }
+      a * {
+        color: #066B64;
+        text-decoration: none;
+      }
+      .post-content a {
+        color: #111827;
+        text-decoration: underline !important;
+        font-weight: bold !important;
+      }
+
+      body {
+        color: #111827;
+      }
+
+      .post-content > div,
+      .post-content > p,
+      .post-content > span,
+      .post-content li {
+        font-family: Helvetica, Roboto, Arial, Verdana, Tahoma, sans-serif;
+        line-height: 28px;
+      }
+
+      .post-content > p,
+      .image-figure,
+      .email-quote {
+        margin-bottom: 24px;
+      }
+
+
+      .creator-image {
+        border-radius: 50%;
+      }
+
+      .post-title {
+        font-family: Helvetica, Roboto, Arial, Verdana, Tahoma, sans-serif;
+        font-size: 36px;
+        font-weight: 700;
+        line-height: 40px;
+      }
+
+      h2 {
+        font-size: 24px;
+        line-height: 28px;
+        margin-bottom: 16px;
+        letter-spacing: 0;
+        margin-top: 48px;
+      }
+
+      h3 {
+        font-size: 18px;
+        line-height: 22px;
+        margin-top: 32px;
+        letter-spacing: 0;
+        margin-bottom: 16px;
+      }
+
+      .image-caption {
+        color: #6B7280;
+        font-size: 14px;
+        line-height: 1.4;
+        letter-spacing: 0.02em;
+      }
+    `
   }
 }
