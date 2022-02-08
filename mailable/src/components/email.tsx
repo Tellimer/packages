c5286dbab53a322b02db1f31acf991ca178e99ed
Tim Feid <tim@timfeid.com>
2022-02-08 17:22:59 -0500

    fix: one more time

--- mailable/src/components/email.tsx
+++ mailable/src/components/email.tsx
@@ -14,46 +14,42 @@ export class Email extends Component<Props> {
   render() {
     const width = this.props.width || 600
     return (
-      <table width={width}>
-        <tr>
-          <td align="center" width={width - 32}>
-            <p className="email-preheader-text">{this.props.preheaderText}</p>
-            <table className="email-container" role="presentation">
-              {this.props.header && (
-                <tr>
-                  <td align="center">
-                    <table role="presentation" width={width - 32} className="email-header">
-                      <tr>
-                        <td>{this.props.header}</td>
-                      </tr>
-                    </table>
-                  </td>
-                </tr>
-              )}
-              <tr>
-                <td align="center">
-                  <EmailTable className="email-body" width={width - 32}>
-                    <EmailRow>
-                      <EmailCol className="email-content">{this.props.children}</EmailCol>
-                    </EmailRow>
-                  </EmailTable>
-                </td>
-              </tr>
-              {this.props.footer && (
-                <tr>
-                  <td align="center">
-                    <table role="presentation" width={width - 32} className="email-footer">
-                      <tr>
-                        <td>{this.props.footer}</td>
-                      </tr>
-                    </table>
-                  </td>
-                </tr>
-              )}
-            </table>
-          </td>
-        </tr>
-      </table>
+      <>
+        <p className="email-preheader-text">{this.props.preheaderText}</p>
+        <EmailTable className="email-container" width={width}>
+          {this.props.header && (
+            <EmailRow>
+              <EmailCol center>
+                <table role="presentation" width={width - 32} className="email-header">
+                  <EmailRow>
+                    <EmailCol>{this.props.header}</EmailCol>
+                  </EmailRow>
+                </table>
+              </EmailCol>
+            </EmailRow>
+          )}
+          <EmailRow>
+            <EmailCol center>
+              <EmailTable className="email-body" width={width - 32}>
+                <EmailRow>
+                  <EmailCol className="email-content">{this.props.children}</EmailCol>
+                </EmailRow>
+              </EmailTable>
+            </EmailCol>
+          </EmailRow>
+          {this.props.footer && (
+            <EmailRow>
+              <EmailCol center>
+                <table role="presentation" width={width - 32} className="email-footer">
+                  <EmailRow>
+                    <EmailCol>{this.props.footer}</EmailCol>
+                  </EmailRow>
+                </table>
+              </EmailCol>
+            </EmailRow>
+          )}
+        </EmailTable>
+      </>
     )
   }
 }
